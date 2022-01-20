54e84115525f812daa4c74beab2ca81d818c4103
izabela.gorzela <izabela.m.gorzela@gmail.com>
2022-01-20 14:14:39 +0100

    The csv conversion functionality to the SimpleEntity objects list has been added

--- src/main/java/BootClass.java
+++ src/main/java/BootClass.java
@@ -1,8 +1,14 @@
-import java.time.LocalDate;
+import java.util.List;
 
 public class BootClass {
 
     public static void main(String[] args) {
 
+        CsvReader csvReader = new CsvReader(args[0]);
+        List<SimpleEntity>  simpleEntityList = new ConverterToSimpleEntity(csvReader.readFromCsv()).convertToSimpleEntity();
+
+        for(SimpleEntity se : simpleEntityList) {
+            System.out.println(se.getId() + " " + se.getName() + " " + se.getDate());
+        }
     }
 }
--- src/main/java/SimpleEntity.java
+++ src/main/java/SimpleEntity.java
@@ -1,11 +1,12 @@
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.ToString;
 
 import java.time.LocalDate;
 
-@ToString
 @AllArgsConstructor
-public class SimpleEntityClass {
+@Getter
+public class SimpleEntity {
 
     private Integer id;
     private String name;
