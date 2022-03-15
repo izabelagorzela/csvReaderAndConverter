71ee25988886610e93005cf00a17e621e77e4633
izabela.gorzela <izabela.m.gorzela@gmail.com>
2022-03-15 22:32:23 +0100

    Fields name in SimpleEntityPrinter changed

--- src/main/java/org/izag/csvreaderandconverter/SimpleEntityPrinter.java
+++ src/main/java/org/izag/csvreaderandconverter/SimpleEntityPrinter.java
@@ -4,18 +4,18 @@ import java.util.List;
 
 public class SimpleEntityPrinter {
 
-    private Formatter simpleEnityFormater;
-    private Filter simpleEntityFilter;
+    private Formatter formatter;
+    private Filter filter;
 
-    public SimpleEntityPrinter(Formatter simpleEntityFormatter, Filter simpleEntityFilter) {
-        this.simpleEnityFormater = simpleEntityFormatter;
-        this.simpleEntityFilter = simpleEntityFilter;
+    public SimpleEntityPrinter(Formatter formatter, Filter filter) {
+        this.formatter = formatter;
+        this.filter = filter;
     }
 
     public void printEntities(List<SimpleEntity> entityList) {
         entityList.stream()
-                .filter(simpleEntity -> simpleEntityFilter.filterSimpleEntity(simpleEntity) == true)
-                .map(simpleEntity -> simpleEnityFormater.formatSimpleEntity(simpleEntity))
+                .filter(simpleEntity -> filter.filterSimpleEntity(simpleEntity) == true)
+                .map(simpleEntity -> formatter.formatSimpleEntity(simpleEntity))
                 .forEach(System.out::println);
     }
 }
